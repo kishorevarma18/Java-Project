031ec24f94d993b8f928f173d2b0768430b484f2
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-04-15 13:45:26 -0400

    added accurate explaination in comments

--- src/methods/MethodEx.java
+++ src/methods/MethodEx.java
@@ -2,24 +2,38 @@ package methods;
 
 import java.util.Scanner;
 public class MethodEx {
-    private String name; // even though name and age are private which cannot be access outside the MethodEx. we can still access it indirectly using checkEligibility method as it is a public method.
-    private int age;
+    String name; 
+    /* Instance variable
+    located inside class (outside method), Each object gets its own copy and Stored in heap */
+    private int age;// even though age has private modifier which cannot be access outside the MethodEx. we can still access it indirectly using checkEligibility method as it is a public method.
     private static Scanner sc = new Scanner(System.in); //we are using static here so it belongs to class instead of object. so by using static, each object have the same scanner.
-    //private Scanner sc = new Scanner(System.in)
-    //without static each object has a separate Scanner for it. So, this creates lots of memory and it's hard to close the scanner after use.
-    //if 1 object is using a scanner and if it is closed then another object can't use the scanner. as the once the scanner is close it won't reopen the system.in while running. 
+    /*private Scanner sc = new Scanner(System.in)
+    -without static each object has a separate Scanner for it. So, this creates lots of memory and it's hard to close the scanner after use.
+    -if 1 object is using a scanner and if it is closed then another object can't use the scanner. as the once the scanner is close it won't reopen the system.in while running. 
+    
+    -Shared by all objects, Only one copy exists and Stored in method area.
+    */
+    
     void checkEligibility(){
-        if(age>24)
+        System.out.println("method is called");
+        int eligibleAge = 25;
+        /* local varibale-
+        located inside method, No default value and Stored in stack.
+         */
+        if(age>eligibleAge)
             System.out.println(name+" is eligible to drink.");
         else
             System.out.println(name+" not eligible to drink.");
     }// method example.
 
     MethodEx(){
+        System.out.println("Constructor called.");
         System.out.println("enter user name:");
         name = sc.nextLine();
         System.out.println("enter user age:");
         age = sc.nextInt();
+        sc.nextLine(); // used this here to remove /n which was left from nextInt(). 
+        //for explanation plz refer oneNote.
     }// constructor example.
 
     public static void main(String[] args) {
