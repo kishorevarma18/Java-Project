25c1a57a8a0a5e1878a7cd0a07d88ac0ef3ad553
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-04-23 14:25:57 -0400

    added comments to differeniate the usecases of Runnable and Thread classes

--- src/multithread/Hello.java
+++ src/multithread/Hello.java
@@ -1,11 +1,13 @@
 package multithread;
 //creating a thread using implements Runnable and override run() method to run the thread.
 public class Hello implements Runnable{
-
+// this Runnable class is helpful in situations like when A class extends B class and i want to create a thread for class A --> it is not possible.
+// EX- public class A extends B, Thread ---> leads to error!
+// solution- public class A extends B implements Runnable   ---> correct way.
     @Override
     public void run() {
         for(int i=0;i<100;i++){
-            System.err.println("Hello");
+            System.out.println("Hello");
         }
         
     }
