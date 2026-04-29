6a7b4a29e0361249fc3ab9b00c8f87f6ce71c4b1
kishorevarma18 <84920504+kishorevarma18@users.noreply.github.com>
2026-04-29 11:38:16 -0400

    added common task for all ThreadPools

---
+++ src/executor/fixedthreadpool/Task.java
@@ -0,0 +1,41 @@
+package executor.fixedthreadpool;
+
+/* 
+ * RUNNABLE (The Task Definition):
+ * This class implements Runnable, which represents the "What" to do.
+ * In the ExecutorService model, we separate the logic (Task) 
+ * from the execution mechanism (the Thread Pool).
+ */
+public class Task implements Runnable {
+    private int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        /* 
+         * Inside an ExecutorService, Thread.currentThread() refers to 
+         * one of the reused threads managed by the pool, not a new thread 
+         * created specifically for this instance.
+         */
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Task " + id + " is running on: " + threadName);
+
+        try {
+            // Simulating work
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            /* 
+             * INTERRUPT PROPAGATION:
+             * Very important! If the ExecutorService calls shutdownNow(), 
+             * it sends an interrupt. We catch it here and re-interrupt 
+             * the thread so the Executor knows this thread should stop.
+             */
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Task " + id + " is completed on: " + threadName);
+    }
+}
